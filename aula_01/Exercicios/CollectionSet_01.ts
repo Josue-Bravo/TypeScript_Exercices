7d0b4fde2cd96b183f802a9e950284e0ca97f788
Josué <josuebc8@gmail.com>
2026-01-30 15:35:07 -0300

    Corrigindo código Collection_01.ts

--- aula_01/Exercicios/CollectionSet_01.ts
+++ aula_01/Exercicios/CollectionSet_01.ts
@@ -1,19 +1,19 @@
 import leia = require("readline-sync");
 
 let setcollection: Set<number> = new Set<number>();
-let continuar = true
-let num: number
+let continuar = true;
+let num: number;
 
-console.log("Bem vindo ao programa de coleção Set\n")
-console.log("AVISO! O programa NÃO aceita numeros repetidos\n")
-console.log("Digite 0 para sair do programa.\n")
-
+console.log("Bem vindo ao programa de coleção Set\n");
 
 while(continuar){
-    num = leia.questionInt("Digite um numero para adicionar: ")
-    num === 0 ? continuar = false : setcollection.add(num)
-    console.clear()
+    num = leia.questionInt("Digite um numero para adicionar: ");
+    num === 0 ? continuar = false : setcollection.add(num);
+    console.clear();
 }
 
-console.log("Lista dos dados do set:\n")
-console.table(setcollection)
+// O SEGREDO ESTÁ AQUI: Converter apenas quando terminar de ler os dados
+let ordenado = Array.from(setcollection); 
+
+console.log("Lista dos dados do set (ordenados):\n");
+console.table(ordenado.sort((a, b) => a - b));
